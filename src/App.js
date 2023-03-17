7eb5e91ea27d1efc77f0c982779309cecfd6c882
riswanmsm <msmr1986@gmail.com>
2023-03-17 16:31:24 +0530

    changes in expenditure app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">Expenditure App</header>
+      <ExpenseItem />
     </div>
   );
 }
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,16 @@
+const ExpenseItem = () => {
+  const day = "12 June 2023";
+  return (
+    <div>
+      <div>
+        <p>{day}</p>
+      </div>
+      <div>
+        <h1>Mobile</h1>
+      </div>
+      <div>Rs.30000.00</div>
+    </div>
+  );
+};
+
+export default ExpenseItem;
